1192b157d09c7194bf15a54895d2307db4d7f0ef
iChubai <2311671@mail.nankai.edu.cn>
2025-02-11 18:02:43 +0000

    [UPDATE]v5 v6 v9 and 11

--- demo.py
+++ demo.py
@@ -1,13 +1,6 @@
 from jittoryolo import YOLO
 import argparse
-import debugpy
-# try:
-#     # 5678 is the default attach port in the VS Code debug configurations. Unless a host and port are specified, host defaults to 127.0.0.1
-#     debugpy.listen(("localhost", 9506))
-#     print("Waiting for debugger attach")
-#     debugpy.wait_for_client()
-# except Exception as e:
-#     pass
+
 def main(args):
     model = YOLO(args.model)
     model.eval()
@@ -30,31 +23,3 @@ def parse_args():
 if __name__ == "__main__":
     args = parse_args()
     main(args)
-# from jittoryolo import YOLO
-# import argparse
-# # Load a model
-# def main(args):
-#     model = YOLO(args.model)
-
-#     # Train the model
-#     train_results = model.train(
-#         data="coco8.yaml",  # path to dataset YAML
-#         epochs=100,  # number of training epochs
-#         imgsz=640,  # training image size
-#         device="cpu",  # device to run on, i.e. device=0 or device=0,1,2,3 or device=cpu
-#     )
-
-#     # Evaluate model performance on the validation set
-#     metrics = model.val()
-
-#     # Perform object detection on an image
-#     results = model("jittoryolo/assets/bus.jpg")
-#     results[0].show()
-
-# def parse_args():
-#     parser = argparse.ArgumentParser()
-#     parser.add_argument("--model", type=str, default="yolov10n-msv2.yaml")
-#     return parser.parse_args()
-# if __name__ == "__main__":
-#     args = parse_args()
-#     main(args)
